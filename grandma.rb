ea95ab83f2e5cfa8c1a61def16a65f89d8b60df0
Raveen K Sharma <raveen08@gmail.com>
2020-05-07 19:15:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 speak_to_grandma = "I LOVE YOU GRANDMA!"
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
+if speak_to_grandma
   return "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_grandma.upcase
   return "NO, NOT SINCE 1938!"
