b28700e4aaeaec9357edfdb5ae015ff69ad1642f
Raveen K Sharma <raveen08@gmail.com>
2020-05-07 19:25:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,6 @@
 
 def speak_to_grandma(phrase)
   if phrase == I LOVE YOU GRANDMA!
-    return 
+    return I LOVE YOU TOO PUMPKIN!
   elsif phrase == phrase.upcase
     return HUH?! SPEAK UP, SONNY!
