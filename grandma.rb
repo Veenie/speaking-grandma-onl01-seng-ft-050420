7533f96f59c54ddc9e5719fefe8bc101c1ae6ebd
Raveen K Sharma <raveen08@gmail.com>
2020-05-07 19:33:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == phrase.upcase
-    'HUH?! SPEAK UP, SONNY!'
+    '
   else
-    
+    'HUH?! SPEAK UP, SONNY!'
 end
